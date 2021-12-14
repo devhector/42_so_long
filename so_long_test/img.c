12ff461b4acb5d80f507e1482b4e3ebeb6d8b6d3
Hector Fernandes <hectorhu17@gmail.com>
2021-12-14 20:48:12 -0300

    update

--- so_long_test/img.c
+++ so_long_test/img.c
@@ -7,12 +7,14 @@ int main()
 		void *win;
 		void *img;
 
+		char *ima = "../img/img.xpm";
+
 		int		img_width;
 		int 	img_height;
 
 		mlx = mlx_init();
 		win = mlx_new_window(mlx, 500, 500, "my_mlx");
-		img = mlx_xpm_file_to_image(mlx, "../img/img.xpm", &img_width, &img_height);
+		img = mlx_xpm_file_to_image(mlx, ima, &img_width, &img_height);
 		mlx_put_image_to_window(mlx, win, img, 100, 100);
 		mlx_loop(mlx);
 		return (0);
