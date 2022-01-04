1663cdace340edb6929948b9430913e88124d464
devhector <dev.hectorfernandes@gmail.com>
2022-01-04 16:03:35 +0100

    mess

--- so_long_test/readfile/get_next_line.c
+++ so_long_test/readfile/get_next_line.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft.h"
 
 static char	*get_line(char	*backup);
 static char	*remove_read_line(char *backup);
--- so_long_test/readfile/readfile.c
+++ so_long_test/readfile/readfile.c
@@ -1,14 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readfile.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 23:08:49 by hectfern          #+#    #+#             */
+/*   Updated: 2022/01/03 23:14:01 by hectfern         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+#include "libft.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-char **map_generator(char *path)
+char	**map_generator(char *path)
 {
-	int fd;
-	char *map;
-	char *temp;
-	char **map_array;
+	int		fd;
+	char	*map;
+	char	*temp;
+	char	**map_array;
 
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
@@ -18,21 +31,25 @@ char **map_generator(char *path)
 	}
 	temp = NULL;
 	map = ft_strdup("");
-
-	while (!!(temp = get_next_line(fd)))
+	while (42)
 	{
+		temp = get_next_line(fd);
+		if (!temp)
+			break ;
 		map = ft_strjoin(map, temp);
 		free(temp);
 	}
 	close(fd);
 	map_array = ft_split(map, '\n');
+	free(map);
 	return (map_array);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	char **map;
-	int i;
+	char	**map;
+	int		count;
+	int		i;
 
 	if (argc != 2)
 	{
@@ -46,5 +63,9 @@ int main(int argc, char **argv)
 		printf("%s\n", map[i]);
 		i++;
 	}
+	count = 0;
+	while (map[count])
+		free(map[count++]);
+	free(map);
 	return (0);
 }
