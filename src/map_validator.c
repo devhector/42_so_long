1c4beb240a45aadf4f4d38d94bedad782948f956
Hector Fernandes <hectorhu17@gmail.com>
2022-01-12 20:39:44 -0300

    fix triangle

--- src/map_validator.c
+++ src/map_validator.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 16:31:30 by hectfern          #+#    #+#             */
-/*   Updated: 2022/01/12 20:07:35 by hectfern         ###   ########.fr       */
+/*   Updated: 2022/01/12 20:12:58 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,21 @@
 
 int	is_rectangle(t_game *game)
 {
-	int	rows;
-	int	cols;
+	int	colums;
+	int	temp;
 	int	x;
-	int	y;
 
-	y = 0;
-	rows = game->map_num_rows / TILE_SIZE;
-	cols = game->map_num_cols / TILE_SIZE;
-	while (game->map[x][y++] != '\0')
-	{
-		x = 0;
-		while (game->map[x++][y] != '\0')
-		{
-			if(x != rows)
-			{
-				printf("Error\nInvalid map\n");
-				return (0);
-			}
-			printf("%d\n", x);
-		}
-	}
 	x = 0;
-	while (game->map[x++][0] != '\0')
+	colums = game->map_num_cols / TILE_SIZE;
+	while (x < game->map_num_rows)
 	{
-		if (ft_strlen(game->map[x]) != cols)
+		temp = ft_strlen(game->map[x]);
+		if (temp != colums)
 		{
-			printf("Error\nInvalid map\n");
+			printf("Error\nMap should be rectangle\n");
 			return (0);
 		}
 	}
-	return (1);
 }
 
 int	wall_validator(t_game *game)
@@ -57,7 +41,6 @@ int	wall_validator(t_game *game)
 	x = 0;
 	y = 0;
 	find_img_size(game);
-	is_rectangle(game);
 	rows = (game->map_num_rows / TILE_SIZE) - 1;
 	colums = (game->map_num_cols / TILE_SIZE) - 1;
 	while (game->map[0][y] == '1' && game->map[rows][y] == '1' && y <= colums)
@@ -84,6 +67,8 @@ int	map_validator(t_game *game)
 	end_position = game->end_position;
 	collectibles = game->collectibles;
 	if (!wall_validator(game))
+		return (0);
+	if (!is_rectangle(game))
 		return (0);
 	if (start_position != 1 || end_position != 1 || collectibles < 1)
 	{
