9258945e738bd23ebed7d743e270f61e3f8ae435
devhector <dev.hectorfernandes@gmail.com>
2022-01-12 20:19:33 +0100

    create triangle func

--- src/map_validator.c
+++ src/map_validator.c
@@ -6,7 +6,7 @@
 /*   By: hectfern <hectfern@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/07 16:31:30 by hectfern          #+#    #+#             */
-/*   Updated: 2022/01/11 23:35:37 by hectfern         ###   ########.fr       */
+/*   Updated: 2022/01/12 20:07:35 by hectfern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,21 @@ int	is_rectangle(t_game *game)
 	y = 0;
 	rows = game->map_num_rows / TILE_SIZE;
 	cols = game->map_num_cols / TILE_SIZE;
-	while (game->map[x][y++])
+	while (game->map[x][y++] != '\0')
 	{
 		x = 0;
-		while (game->map[x++][y])
+		while (game->map[x++][y] != '\0')
+		{
 			if(x != rows)
 			{
 				printf("Error\nInvalid map\n");
 				return (0);
 			}
+			printf("%d\n", x);
+		}
 	}
 	x = 0;
-	while (game->map[x++])
+	while (game->map[x++][0] != '\0')
 	{
 		if (ft_strlen(game->map[x]) != cols)
 		{
